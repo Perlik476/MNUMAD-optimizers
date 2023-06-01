3841aae47464a693632724ace2249ac1dd15ff9d
Perlik476 <mp429581@students.mimuw.edu.pl>
2023-06-01 22:03:16 +0200

    Implement Levenberg-Marquardt method

--- project/lib.py
+++ project/lib.py
@@ -1,40 +1,67 @@
+from typing import Callable
+
 import numpy as np
 
-def gradient_descent(F: callable, DF: callable, p0: np.ndarray, alpha: float, max_iter: int) -> np.ndarray:
-    """
-    Gradient descent algorithm for unconstrained optimization.
-    :param f: function to be minimized
-    :param df: gradient of f
-    :param x0: initial point
-    :param alpha: learning rate
-    :param max_iter: maximum number of iterations
-    :return: minimum point
-    """
 
-    assert alpha > 0, "alpha must be positive"
-    assert max_iter > 0, "max_iter must be positive"
-
-    p = p0
-    for _ in range(max_iter):
-        p = p - alpha * DF(p)
-    return p
-
-
-def gauss_newton(F: callable, DF: callable, p0: np.ndarray, max_iter: int) -> np.ndarray:
+def gauss_newton(
+    F: Callable, DF: Callable, p0: np.ndarray, max_iter: int, silent: bool = True
+) -> tuple[np.ndarray, np.float64]:
     """
     Gauss-Newton algorithm for unconstrained optimization.
     :param f: function to be minimized
     :param df: gradient of f
     :param x0: initial point
-    :param alpha: learning rate
     :param max_iter: maximum number of iterations
     :return: minimum point
     """
+    assert max_iter > 0, "max_iter must be positive"
+
     p = p0
+    err = np.linalg.norm(F(p))
+
     for i in range(max_iter):
-        print(f"iter {i}: p = {p}, ||F(p)|| = {np.linalg.norm(F(p))}")
+        err = np.linalg.norm(F(p))
+        if not silent:
+            print(f"iter {i}: p = {p}, ||F(p)|| = {err}")
         try:
-            p = p - np.linalg.solve(DF(p).T @ DF(p),  DF(p).T @ F(p))
+            p = p - np.linalg.solve(DF(p).T @ DF(p), DF(p).T @ F(p))
         except np.linalg.LinAlgError:
-            return p
-    return p
+            return p, err
+
+    return p, err
+
+
+def levenberg_marquardt(
+    lambda_fun: Callable,
+    F: Callable,
+    DF: Callable,
+    p0: np.ndarray,
+    max_iter: int,
+    silent: bool = True,
+) -> tuple[np.ndarray, np.float64]:
+    """
+    Levenberg-Marquardt algorithm for unconstrained optimization.
+    :param f: function to be minimized
+    :param df: gradient of f
+    :param x0: initial point
+    :param max_iter: maximum number of iterations
+    :return: minimum point
+    """
+    assert max_iter > 0, "max_iter must be positive"
+
+    p = p0
+    err = np.linalg.norm(F(p))
+
+    for i in range(max_iter):
+        err = np.linalg.norm(F(p))
+        if not silent:
+            print(f"iter {i}: p = {p}, ||F(p)|| = {err}")
+        try:
+            p = p - np.linalg.solve(
+                DF(p).T @ DF(p) + lambda_fun(F, DF, p, i) * np.eye(p.size),
+                DF(p).T @ F(p),
+            )
+        except np.linalg.LinAlgError:
+            return p, err
+
+    return p, err
